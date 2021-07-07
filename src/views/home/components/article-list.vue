c5a72278a6a157037c3185f2357ef64dd1040d32
zep03 <910456393@qq.com>
2021-07-07 20:33:29 +0800

    '文章列表下拉刷新'

---
+++ src/views/home/components/article-list.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="article-list">
+      <van-pull-refresh :success-duration="1500" :success-text="refreshSuccessText" v-model="isRefreshLoading" @refresh="onRefresh">
+        <van-list
+          v-model="loading"
+          :finished="finished"
+          finished-text="没有更多了"
+          @load="onLoad"
+        >
+          <van-cell v-for="(article, index) in articles" :key="index" :title="article.title" />
+        </van-list>
+      </van-pull-refresh>
+    </div>
+</template>
+
+<script>
+import { getArticles } from '../../../api/article'
+
+export default {
+  name: 'ArticleList',
+  data () {
+    return {
+      articles: [], // 数据列表
+      loading: false, // 控制加载中的loading状态
+      finished: false, // 控制加载结束的状态，当加载结束，不再触发加载更多
+      timestamp: null, // 获取下一页数据的时间戳
+      isRefreshLoading: false, // 下拉刷新的loading状态
+      refreshSuccessText: '' // 下拉刷新成功的提示文本
+    }
+  },
+  props: {
+    channel: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    async onLoad () {
+      // 1. 请求获取数据
+      const { data } = await getArticles({
+        channel_id: this.channel.id, // 频道的id
+        timestamp: this.timestamp || Date.now(), // 时间戳，请求新的推荐数据传当前的时间戳，请求历史推荐传指定的时间戳
+        // timestamp相当于页码，请求最新数据使用当前的时间戳，请求下一页数据，使用上一次返回的数据的时间戳
+        with_top: 1 // 是否包含置顶，进入页面第一次请求时要包含置顶文章，1-包含置顶，0-不包含
+      })
+      console.log(data)
+      // 2. 把数据放到articles数组中
+      const { results } = data.data
+      this.articles.push(...results)
+      // 3. 设置本次加载状态结束，他才可以判断是否需要加载下一次，否则永远都会停在这里
+      this.loading = false
+      // 4. 数据全部加载完成
+      if (results.length) {
+        // 更新获取下一页数据的页码
+        this.timestamp = data.data.pre_timestamp
+      } else {
+        // 没有数据了，把加载状态设置为结束，不在触发加载更多
+        this.finished = true
+      }
+    },
+    async onRefresh () {
+      // 下拉刷新，组件自己就会展示loading状态
+      // 1. 请求获取数据
+      const { data } = await getArticles({
+        channel_id: this.channel.id, // 频道的id
+        timestamp: Date.now(), // 时间戳，请求新的推荐数据传当前的时间戳，请求历史推荐传指定的时间戳
+        // timestamp相当于页码，请求最新数据使用当前的时间戳，请求下一页数据，使用上一次返回的数据的时间戳
+        with_top: 1 // 是否包含置顶，进入页面第一次请求时要包含置顶文章，1-包含置顶，0-不包含
+      })
+      // 2. 把数据往数组的前面追加
+      const { results } = data.data
+      this.articles.unshift(...results)
+      // 3. 关闭刷新的状态loading
+      this.isRefreshLoading = false
+      this.refreshSuccessText = `更新了${results.length}条数据`
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+  .article-list {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 50px;
+    top: 90px;
+    overflow-y: auto;
+  }
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,66 @@
 <template>
     <div class="home-container">
-      home
+      <!--导航栏-->
+      <van-nav-bar
+        class="app-nav-bar">
+        <van-button class="search-btn" slot="title" icon="search" size="small" type="info" round>搜索</van-button>
+      </van-nav-bar>
+      <!--文章频道列表-->
+      <!--
+        标签页组件有一个功能，只有你第1次查看标签页的时候，才会渲染里面的内容
+      -->
+      <van-tabs v-model="active">
+        <van-tab v-for="channel in channels" :title="channel.name" :key="channel.id">
+          <!--文章列表-->
+          <article-list :channel="channel"></article-list>
+        </van-tab>
+      </van-tabs>
     </div>
 </template>
 
 <script>
+import { getUserChannels } from '../../api/user.js'
+import ArticleList from './components/article-list'
 export default {
-  name: 'HomeIndex'
+  name: 'HomeIndex',
+  components: {
+    ArticleList
+  },
+  data () {
+    return {
+      active: 0, // 控制被激活的标签
+      channels: [] // 频道列表
+    }
+  },
+  created () {
+    this.loadChannels()
+  },
+  methods: {
+    async loadChannels () {
+      const res = await getUserChannels()
+      this.channels = res.data.data.channels
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
 
+  .home-container {
+    /deep/ .van-nav-bar__title {
+      max-width: unset;
+    }
+    .search-btn {
+      width: 277px;
+      height: 32px;
+      background-color: #5babfb;
+      border: none;
+      .van-button__text {
+        font-size: 14px;
+      }
+      .van-icon {
+        font-size: 16px;
+      }
+    }
+  }
 </style>
