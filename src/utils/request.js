5e1410cde2eaa7fdd0c6d1cbfb0b62ecce6eec6d
zep03 <910456393@qq.com>
2021-07-24 15:42:52 +0800

    '处理token过期问题'

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,6 +5,8 @@ import axios from 'axios'
 // 在非组件模块中获取store 必须采用这种方式
 import store from '../store/index.js'
 import JSONbig from 'json-bigint'
+import { Toast } from 'vant'
+import router from '../router'
 
 const request = axios.create({
   baseURL: '/api', // 基础路径
@@ -29,6 +31,9 @@ const request = axios.create({
   }]
 })
 
+const refreshTokenReq = axios.create({
+  baseURL: '/api' // 基础路径
+})
 // 请求拦截器
 request.interceptors.request.use(function (config) {
   const { user } = store.state
@@ -43,6 +48,58 @@ request.interceptors.request.use(function (config) {
 })
 
 // 响应拦截器
+request.interceptors.response.use(function (response) {
+  // 响应成功进入这里
+  return response
+}, async function (error) {
+  // 响应失败进入这里
+  const status = error.response.status
+  if (status === 400) {
+    // 客户端请求参数错误
+    Toast.fail('客户端请求参数错误')
+  } else if (status === 401) {
+    // token无效
+    // 如果没有 user 或者 user.token， 直接去登录
+    const { user } = store.state
+    if (!user || !user.token) {
+      // 直接跳转到登录页面
+      return redirectLogin()
+    }
+    // 使用refresh_token,则请求获取新的token
+    try {
+      const { data } = await refreshTokenReq({
+        method: 'PUT',
+        url: '/app/v1_0/authorizations',
+        headers: {
+          Authorization: `Bearer ${user.refresh_token}`
+        }
+      })
+      // 拿到新的token之后把它更新到容器中
+      user.token = data.data.token
+      store.commit('setUser', user)
+      // 把失败的请求重新发送出去
+      // error.config 是本次请求的相关配置项
+      // 注意： 这里使用request发请求，它会走自己的请求拦截器，它的请求
+      //  拦截器中通过store容器访问token数据
+      return request(error.config)
+      // console.log(data)
+    } catch (err) {
+      // 刷新token都失败了，直接跳转到登录页
+      redirectLogin()
+    }
+  } else if (status === 403) {
+    // 没有权限操作
+    Toast.fail('没有权限操作')
+  } else if (status >= 500) {
+    // 服务端异常
+    Toast.fail('服务端异常，请稍后重试！')
+  }
+  // console.dir(error)
+  return Promise.reject(error)
+})
+function redirectLogin () {
+  router.replace('/login')
+}
 
 // 导出
 export default request
